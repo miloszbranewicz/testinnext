5fbc4a2dcae434cbf30e387b13e99e08908c1b22
Miloszel <m.branewicz@wp.pl>
2023-11-04 19:13:48 +0100

    [FIX] Lint errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
 
   return (
     <>
@@ -44,9 +44,7 @@ export default function Home() {
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : "Loading tRPC query..."}
-            </p>
+
             <AuthShowcase />
           </div>
         </div>
